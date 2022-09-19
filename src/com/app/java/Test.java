77bdb4eceb1d408d846a928c9f73c93ec68b6e70
anudeep5e7 <anudeeprachamalla@gmail.com>
2022-09-19 22:43:37 +0530

    Testcmt

---
+++ src/com/app/java/Test.java
@@ -0,0 +1,16 @@
+package com.app.java;
+
+public class Test {
+
+	public void show(String s) {
+
+		System.out.println("welcome to java");
+	}
+
+	public static void main(String[] args) {
+
+		Test t = new Test();
+
+		t.show("anudeep");
+	}
+}
